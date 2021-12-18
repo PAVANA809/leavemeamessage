43f349812cc07d1f9eceaf46875f92a1d4921981
PAVANA809 <pavanhapavan@gmail.com>
2021-12-18 18:52:51 +0530

    testing

--- lmam_bot.py
+++ lmam_bot.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
-CHAT_ID = os.getenv('CHAT_ID')
+# CHAT_ID = os.getenv('CHAT_ID')
 
-bot = telebot.TeleBot('5004963820:AAEKRFFX-H7hj8Nz9MM4HQROlPhfqshHAt8')
+bot = telebot.TeleBot(API_KEY)
 
 def sendmessage(chat_id,msg):
     bot.send_message(chat_id, msg)
